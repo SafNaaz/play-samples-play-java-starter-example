45cbfee62dd48a9f38d2ab1d788d646177271e2b
Safnas <iamsafnas@gmail.com>
2020-07-25 13:38:45 +0530

    Create BooksController.java
    create Action methods skeleton

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+    //show all books
+    public Result index(){
+        return TODO;
+    }
+
+    // to create book
+    public Result create(){
+        return TODO;
+    }
+
+    // to save book
+    public Result save(){
+        return TODO;
+    }
+
+    //edit one book
+    public Result edit(Integer id){
+        return TODO;
+    }
+
+    //update in database
+    public Result update(){
+        return TODO;
+    }
+
+    //delete book
+    public Result destroy(Integer id){
+        return TODO;
+    }
+
+    //details of single book
+    public Result details(){
+        return TODO;
+    }
+}
