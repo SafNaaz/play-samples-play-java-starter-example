455009a14ee64a2564fcb72ca382c5f987a720dd
Safnas <iamsafnas@gmail.com>
2020-07-26 02:26:40 +0530

    updated model with ORM annotation and Finder method

--- app/models/Book.java
+++ app/models/Book.java
@@ -1,83 +1,19 @@
 package models;
 
-import java.util.HashSet;
-import java.util.Set;
+import io.ebean.Finder;
+import io.ebean.Model;
 
-public class Book {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Book extends Model {
+
+    @Id
     public Integer id;
     public String title;
     public Integer price;
     public String author;
 
-    public Book(){
-    }
-
-    public Book(Integer id, String title, Integer price, String author) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
-        this.author = author;
-    }
-
-    private static Set<Book> books;
-
-    static {
-        books = new HashSet<>();
-        books.add(new Book(1,"C++",20,"ABC"));
-        books.add(new Book(2,"Java",30,"XYZ"));
-    }
-
-    public static Set<Book> allBooks(){
-        return books;
-    }
-
-    public static Book findById(Integer id){
-        for (Book book : books){
-            if(id.equals(book.id)){
-                return book;
-            }
-        }
-        return null;
-    }
-
-    public static void add(Book book){
-        books.add(book);
-    }
-
-    public static boolean remove(Book book){
-        return books.remove(book);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    public static Finder<Integer,Book> find = new Finder<>(Book.class);
 }
