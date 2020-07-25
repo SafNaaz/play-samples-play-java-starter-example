af00c548d1daa59497085d5ed78c06b94c00a021
Safnas <iamsafnas@gmail.com>
2020-07-25 14:05:02 +0530

    added routes and not implemented status sent

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -1,42 +1,44 @@
 package controllers;
 
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
 
 public class BooksController extends Controller {
 
     //show all books
     public Result index(){
-        return TODO;
+        return status(NOT_IMPLEMENTED);
     }
 
     // to create book
     public Result create(){
-        return TODO;
+        return status(NOT_IMPLEMENTED);
     }
 
     // to save book
     public Result save(){
-        return TODO;
+        return status(NOT_IMPLEMENTED);
     }
 
     //edit one book
     public Result edit(Integer id){
-        return TODO;
+        return status(NOT_IMPLEMENTED);
     }
 
     //update in database
     public Result update(){
-        return TODO;
+        return status(NOT_IMPLEMENTED);
     }
 
     //delete book
     public Result destroy(Integer id){
-        return TODO;
+        return status(NOT_IMPLEMENTED);
     }
 
     //details of single book
-    public Result details(){
-        return TODO;
+    public Result show(Integer id){
+        return status(NOT_IMPLEMENTED);
     }
+
 }
