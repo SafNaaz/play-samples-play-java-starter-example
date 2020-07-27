36c263d28c01c85dd61753ed4b0ebb95400dabbc
Safnas <iamsafnas@gmail.com>
2020-07-27 14:21:37 +0530

    Update TestCountController.java

--- app/controllers/TestCountController.java
+++ app/controllers/TestCountController.java
@@ -1,36 +1,36 @@
-package controllers;
+// package controllers;
 
-import play.mvc.Controller;
-import play.mvc.Result;
-import services.Counter;
+// import play.mvc.Controller;
+// import play.mvc.Result;
+// import services.Counter;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
+// import javax.inject.Inject;
+// import javax.inject.Singleton;
 
-/**
- * This controller demonstrates how to use dependency injection to
- * bind a component into a controller class. The class contains an
- * action that shows an incrementing count to users. The {@link Counter}
- * object is injected by the Guice dependency injection system.
- */
-@Singleton
-public class TestCountController extends Controller {
+// /**
+//  * This controller demonstrates how to use dependency injection to
+//  * bind a component into a controller class. The class contains an
+//  * action that shows an incrementing count to users. The {@link Counter}
+//  * object is injected by the Guice dependency injection system.
+//  */
+// @Singleton
+// public class TestCountController extends Controller {
 
-    private final Counter counter;
+//     private final Counter counter;
 
-    @Inject
-    public TestCountController(Counter counter) {
-        this.counter = counter;
-    }
+//     @Inject
+//     public TestCountController(Counter counter) {
+//         this.counter = counter;
+//     }
 
-    /**
-     * An action that responds with the {@link Counter}'s current
-     * count. The result is plain text. This action is mapped to
-     * <code>GET</code> requests with a path of <code>/count</code>
-     * requests by an entry in the <code>routes</code> config file.
-     */
-    public Result count() {
-        return ok(Integer.toString(counter.nextCount()));
-    }
+//     /**
+//      * An action that responds with the {@link Counter}'s current
+//      * count. The result is plain text. This action is mapped to
+//      * <code>GET</code> requests with a path of <code>/count</code>
+//      * requests by an entry in the <code>routes</code> config file.
+//      */
+//     public Result count() {
+//         return ok(Integer.toString(counter.nextCount()));
+//     }
 
-}
+// }
